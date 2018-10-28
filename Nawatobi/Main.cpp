f11232a28049bed989b1c91e497a336e9b8077d6
守 小室 <hipukon2@gmail.com>
2018-10-28 21:39:45 +0900

    lamda use

--- Nawatobi/Main.cpp
+++ Nawatobi/Main.cpp
@@ -9,69 +9,51 @@ enum class State
 	Result,
 };
 
-struct CommonData
-{
-	State scene = State::Opening;
-	Font font {50};
-	int32 score=0;
-};
-
-void Opening(CommonData& common)
-{
-	common.font(U"なわとびげええええむ").drawAt(Window::Center());
-
-
-	if (MouseL.down())
-	{
-		common.scene = State::Game;
-	}
-}
-
-void Game(CommonData& common)
-{
-	Rect(Window::Size()).draw(Palette::Skyblue);
-	Rect(Size(0, 400), Size(Window::Size().x, 300)).draw(Palette::Gray);
-	if (MouseR.down())
-	{
-		common.scene = State::Opening;
-	}
-	if(MouseL.down())
-	{
-		common.scene = State::Result;
-	}
-
-	if(KeySpace.down())
-	{
-		++common.score;
-	}
-}
-
-void Result(CommonData& common)
-{
-	common.font(U"スコアは", common.score, U"").drawAt(Window::Center());
-}
-
 void Main()
 {
 
-	CommonData common;
+	State scene = State::Opening;
+	Font font{ 50 };
+	int32 score = 0;
+
+	const auto Opening = [&]()
+	{
+		font(U"なわとびげええええむ").drawAt(Window::Center());
+
+
+		if (MouseL.down())
+		{
+			scene = State::Game;
+		}
+	};
+
+	const auto Game = [&]()
+	{
+		Rect(Window::Size()).draw(Palette::Skyblue);
+		Rect(Size(0, 400), Size(Window::Size().x, 300)).draw(Palette::Gray);
+		if (MouseR.down())
+		{
+			scene = State::Opening;
+		}
+		if (MouseL.down())
+		{
+			scene = State::Result;
+		}
+
+		if (KeySpace.down())
+		{
+			++score;
+		}
+	};
+	const auto Result=[&]()
+	{
+		font(U"スコアは", score, U"").drawAt(Window::Center());
+	};
+
+	Array<std::function<void()>> functions{Opening, Game, Result};
 
 	while (System::Update())
 	{
-		switch (common.scene)
-		{
-		case State::Opening:
-		{
-			Opening(common);
-			break;
-		}
-		case State::Game:
-			Game(common);
-			break;
-		case State::Result:
-			Result(common);
-			break;
-		default: ;
-		}
+		functions[static_cast<size_t>(scene)]();
 	}
 }
